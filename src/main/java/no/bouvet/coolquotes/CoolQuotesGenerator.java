02054716e3bf9e3ab6c1431fad135fefaec21cc2
Idar Borlaug <idar.borlaug@gmail.com>
2010-09-21 20:41:31 +0200

    generates random quote

--- src/main/java/no/bouvet/coolquotes/CoolQuotesGenerator.java
+++ src/main/java/no/bouvet/coolquotes/CoolQuotesGenerator.java
@@ -1,11 +1,19 @@
 package no.bouvet.coolquotes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service("coolQuotesGenerator")
 public class CoolQuotesGenerator implements CoolQuotes{
 
+    @Autowired
+    private CoolQuotesRepository repository;
+
+    private Random generator = new Random();
+
     public String getRandomQuote(){
-        return "";
+        return repository.getQuote(generator.nextInt(repository.getSize()));
     }
 }
