8b6bbb9eddab3ec4786f07127ea642d636122541
Idar Borlaug <idar.borlaug@gmail.com>
2010-09-21 19:46:01 +0200

    Step 2 created business value

---
+++ src/main/java/no/bouvet/coolquotes/CoolQuotesRepository.java
@@ -0,0 +1,24 @@
+package no.bouvet.coolquotes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoolQuotesRepository {
+
+    private List<String> storage = new ArrayList<String>();
+
+    public CoolQuotesRepository() {
+        storage.add("A dog is the only thing on earth that loves you more than you love yourself.");
+        storage.add("A child of five would understand this. Send someone to fetch a child of five.");
+        storage.add("A friend doesn't go on a diet because you are fat.");
+        storage.add("A word to the wise ain't necessary - it's the stupid ones that need the advice.");
+    }
+
+    public String getQuote(int i){
+        return storage.get(i);
+    }
+
+    public int getSize(){
+        return storage.size();
+    }
+}
