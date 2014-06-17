9065aee61e22fec7bf13d536dcd6597be87c95eb
ayhero <ayhero@yeah.net>
2014-06-17 17:33:53 +0800

    --增加注释

--- src/main/java/service/test/ServiceTest.java
+++ src/main/java/service/test/ServiceTest.java
@@ -10,16 +10,16 @@ import wss.SpringService;
 public class ServiceTest {
 
 	public static void main(String[] args) throws Exception {
-		
+		//客户端动态实现
 		JaxWsDynamicClientFactory clientFactory = JaxWsDynamicClientFactory.newInstance();
 		Client client = clientFactory.createClient("http://localhost:8080/webservice/services/wss?wsdl");
 		Object[] result = client.invoke("sayHello", "jo");
 		System.out.println(result[0]);
 
+		//客户端非动态实现
 		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
         factory.setServiceClass(SpringService.class);
         factory.setAddress("http://localhost:8080/webservice/services/wss?wsdl");
-        
         SpringService service = (SpringService) factory.create();
         System.out.println("[result]" + service.sayHello("jo"));
 	}
